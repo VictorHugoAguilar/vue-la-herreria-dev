a967876ca9080c3e65271d95f4512a5ff0513f37
Victor Hugo Aguilar <victorhugoaguilar@aol.com>
2022-11-17 10:07:59 +0100

    Add files via upload

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -9,13 +9,12 @@
       <nuxt-content :document="article" />
       <nuxt-link to="/">
         <div class="volver">
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-              d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" />
+          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="{2}" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" />
           </svg>
         </div>
       </nuxt-link>
+      <Comment />
     </article>
   </div>
 </template>
@@ -26,7 +25,6 @@ export default {
     const article = await $content('blog', params.slug).fetch()
     return { article }
   }
-
 }
 </script>
 
